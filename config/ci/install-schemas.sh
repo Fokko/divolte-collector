16e63e76c9dea54b19747575a64be817ecb22ea9
Fokko Driesprong <fokko@apache.org>
2019-09-27 07:21:12 +0200

    Cleanup and simplify CI job

--- config/ci/install-schemas.sh
+++ config/ci/install-schemas.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-echo "Install Apache Maven"
+set -ex
 
 wget -q -O - http://archive.apache.org/dist/maven/maven-3/3.3.3/binaries/apache-maven-3.3.3-bin.tar.gz | tar -xzf - -C /usr/local \
       && ln -s /usr/local/apache-maven-3.3.3 /usr/local/apache-maven \
       && ln -s /usr/local/apache-maven/bin/mvn /usr/local/bin/mvn
-
-echo "Install the Divolte schema"
 
 git clone --depth 1 https://github.com/divolte/divolte-schema.git /tmp/divolte-schema \
       && cd /tmp/divolte-schema \
